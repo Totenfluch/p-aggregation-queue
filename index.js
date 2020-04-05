6b0e82f89bddbc09cefe5dc64d6a1d256c6431c3
Christian Ziegler <ziegler.christian@kabelmail.de>
2020-04-05 06:35:39 +0200

    PoC ready

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ class QueueClass {
         this._queue = [];
     }
     enqueue(run, options) {
-        options = Object.assign({ priority: 0 }, options);
         const element = {
             options,
             run
@@ -14,12 +13,14 @@ class QueueClass {
             if (typeof options.mergeFn !== 'function') {
                 continue;
             }
-            const result = options.mergePromise(this._queue[i].options, options, options.mergeFn);
-            if (result !== null) {
-                //console.log(result, `merged ${JSON.stringify(options)} WITH ${JSON.stringify(this._queue[i].options)}`);
-                this.removeItemFromQueueIndex(i);
-                this.insertIntoQueue(i, {run: result, options: {}});
-                return;
+            for (let x = 0; x < options.mergePromise.length; x += 1) {
+                const result = options.mergePromise[x](this._queue[i].options, options, options.mergeFn);
+                if (result !== null) {
+                    //console.log(result, `merged ${JSON.stringify(options)} WITH ${JSON.stringify(this._queue[i].options)}`);
+                    this.removeItemFromQueueIndex(i);
+                    this.insertIntoQueue(i, { run: result.fn, options: result.options });
+                    return;
+                }
             }
         }
         this._queue.push(element);
@@ -43,7 +44,7 @@ class QueueClass {
     }
 }
 
-const queue = new PQueue({concurrency: 2, queueClass: QueueClass});
+const queue = new PQueue({concurrency: 1, queueClass: QueueClass});
 
 let count = 0;
 queue.on('active', () => {
@@ -54,34 +55,64 @@ queue.on('active', () => {
     }, 1000);
 });
 
-async function promise1(user, number) {
-    console.log('PROMISE1 CALL: ', user, number, 'END PROMISE 1 CALL');
+async function singletip(from, to, coin, amount) {
+    console.log('SINGLETIP CALL: ', from, to, coin, amount, 'SINGLETIP END');
+    return `TX${from}${to}${coin}${amount}`;
 }
 
-async function promiseAll(combined) {
-    console.log('PROMISE ALL CALL', combined, ' END ALL CALL');
+async function multiTip(from, addresses) {
+    console.log('MULTITIP ALL CALL', from, addresses, ' MULTITIP END');
+    return `TX${from}${addresses}`;
 }
 
-const mergedFnGlobal = promiseAll;
+const mergedFnGlobal = multiTip;
 
-function mergePromise(promise1Call, promise2Call, mergedFn) {
-    if (promise1Call.method === promise2Call.method) {
-        const concatParams = [];
-        concatParams.push({paramsF1: promise1Call, paramsF2: promise2Call});
-        return async () => mergedFn(concatParams);
+function mergeSingle(promise1Call, promise2Call, mergeFn) {
+    if (promise1Call.method === promise2Call.method && promise1Call.from === promise2Call.from) {
+        const toAddresses = {};
+        toAddresses[`${promise1Call.to}`] = promise1Call.amount;
+        toAddresses[`${promise2Call.to}`] = promise2Call.amount;
+        return { 
+            fn: async () => mergeFn(promise1Call.from, toAddresses),
+            options: {
+                method: 'multi',
+                from: promise1Call.from,
+                addresses: toAddresses,
+                mergePromise: [],
+                mergeFn: null,
+            },
+        }
+    }
+    return null;
+}
+
+function mergeToMulti(multiCall, singleCall, mergeFn) {
+    if (multiCall.method === 'multi' && singleCall.method === 'single' && multiCall.from === singleCall.from) {
+        multiCall.addresses[`${singleCall.to}`] = singleCall.amount;
+        return { 
+            fn: async () => mergeFn(multiCall.from, multiCall.addresses), 
+            options: {
+                method: 'multi',
+                from: multiCall.from,
+                addresses: multiCall.addresses,
+                mergePromise: [],
+                mergeFn: null,
+            },
+        };
     }
     return null;
 }
  
 async function run() {
-    queue.add(() => promise1('stefan', 98), {method: 'xy', param1: 'asdf', param2: 99, mergePromise, mergeFn: mergedFnGlobal });
-    queue.add(() => promise1('peter', 98), {method: 'xy', param1: 'asdfj', param2: 98, mergePromise, mergeFn: mergedFnGlobal });
-    queue.add(() => promise1('dofian', 97), {method: 'xy', param1: 'asdfj', param2: 97, mergePromise, mergeFn: mergedFnGlobal });
-    queue.add(() => promise1('stefan', 1), {method: 'promise1', param1: 'stefan', param2: 1, mergePromise, mergeFn: mergedFnGlobal });
-    queue.add(() => promise1('stefan', 2), {method: 'promise1', param1: 'stefan', param2: 2, mergePromise, mergeFn: mergedFnGlobal });
-    queue.add(() => promise1('kevin', 3), {method: 'promise1', param1: 'kevin', param2: 3, mergePromise, mergeFn: mergedFnGlobal });
-    queue.add(() => promise1('kevin', 4), {method: 'promise1', param1: 'kevin', param2: 4, mergePromise, mergeFn: mergedFnGlobal });
-    queue.add(() => promise1('kevin', 5), {method: 'promise1', param1: 'kevin', param2: 5, mergePromise, mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('stefan', 'TaXb1', 'tzc', 10), { method: 'single', from: 'stefan', to: 'TaXb1', amount: 10, mergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('torsten', 'TaXb2', 'tzc', 11), { method: 'single', from: 'torsten', to: 'TaXb2', amount: 11, mmergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('xana', 'TaXb3', 'tzc', 12), { method: 'single', from: 'xana', to: 'TaXb3', amount: 12, mergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('stefan', 'TaXb4', 'tzc', 15), { method: 'single', from: 'stefan', to: 'TaXb4', amount: 15, mergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('xana', 'TaXb5', 'tzc', 16), { method: 'single', from: 'xana', to: 'TaXb5', amount: 16, mergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('stefan', 'TaXb6', 'tzc', 17), { method: 'single', from: 'stefan', to: 'TaXb6', amount: 17, mergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('xana', 'TaXb7', 'tzc', 18), { method: 'single', from: 'xana', to: 'TaXb7', amount: 18, mergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('stefan', 'TaXb8', 'tzc', 19), { method: 'single', from: 'stefan', to: 'TaXb8', amount: 19, mergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
+    queue.add(() => singletip('xana', 'TaXb9', 'tzc', 20), { method: 'single', from: 'xana', to: 'TaXb9', amount: 20, mergePromise: [mergeToMulti, mergeSingle], mergeFn: mergedFnGlobal });
 }
 
 run();
